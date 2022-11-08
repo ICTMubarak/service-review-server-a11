baeaa6b5d4c9e049682ab768d5cd33df7e4ed9b9
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-08 00:11:22 -0800

    One data insert successfully

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+// id: user4
+// pass: wU0VyrI7833RCBc8
 
 //middleware
 app.use(cors());
@@ -11,11 +15,58 @@ app.use(express.json());
 
 
 
+const uri = "mongodb+srv://user4:wU0VyrI7833RCBc8@cluster0.ltjdg3f.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run(){
+
+    try{
+
+         const serviceCollection = client.db('ServiceCollection').collection('services');
+
+        //  app.get('/addservices', async(req, res)=>{
+        //      const query = {};
+        //      const cursor = serviceCollection.find(query);
+        //      const services = await cursor.toArray();
+        //      res.send(services);
+
+        // });
+
+
+         app.post('/addservices', async(req, res) =>{
+             const service = req.body;
+             console.log(service); 
+             const result = await serviceCollection.insertOne(service);
+             res.send(result);
+         });
+
+
+        // app.get('/users/:id', async(req, res) =>{
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id)}
+        //     const result = await userCollection.findOne(query);
+        //     res.send(result);
+         //})
+  
+    }
+
+    finally{
+
+    }
+
+}
+
+run().catch(err => console.log(err));
+
+
 
 
 
 app.get('/', (req, res) =>{
-    res.send('Hello from easy services server');
+    res.send('Hello from easy service server');
 })
 
 app.listen(port, ()=>{
