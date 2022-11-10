650877231d8f9cf6f32007533749f6e4858e59cf
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-10 06:33:39 -0800

    get method work successfully for spacific service detail

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run(){
             
         });
 
-
+        
          app.get('/detailservice/:id', async(req, res) =>{
              const id = req.params.id;
              const query = { _id: ObjectId(id)}
